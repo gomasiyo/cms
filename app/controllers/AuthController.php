7e15c9f5bdc05ddc18510c9863a3872be7cb6392
gomasiyo <goma@goma-gz.com>
2015-12-15 07:35:15 +0900

    Authcontroller 作成     作成のみ

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *  [API] アカウントに関するクラス
+ *
+ *  アカウントに関するAPIをまとめたコントローラー
+ *  エンドポイント単位でメゾットを定義
+ *  Privateメゾットはプリフィックスにアンダーバー[_]をつける
+ *
+ *  @access public
+ *  @author Goma::NanoHa <goma@goma-gz.net>
+ *  @extends ControllerBase
+ */
+
+class AuthController extends ContorollerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+    public function signupAction()
+    {
+
+        
+
+    }
+
+}
+
