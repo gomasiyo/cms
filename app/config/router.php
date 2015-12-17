aa829eb11fadc1a6cbc9290d560ebd593fde7816
gomasiyo <goma@goma-gz.com>
2015-12-18 01:33:01 +0900

    EntryController 追加         Add完成

--- app/config/router.php
+++ app/config/router.php
@@ -10,11 +10,45 @@ $router = new \Phalcon\Mvc\Router(false);
  */
 $router->add(
     '/',
-    array(
+    [
         'controller' => 'Index',
         'action' => 'index'
-    )
+    ]
 );
+
+/**
+ *  /auth/signup
+ */
+$router->addPost(
+    '/auth/signup',
+    [
+        'controller' => 'auth',
+        'action' => 'signup'
+    ]
+);
+
+/**
+ *  /auth/login
+ */
+$router->addPost(
+    '/auth/login',
+    [
+        'controller' => 'auth',
+        'action' => 'login'
+    ]
+);
+
+/**
+ *  /auth/login
+ */
+$router->addPost(
+    '/entry/add',
+    [
+        'controller' => 'entry',
+        'action' => 'add'
+    ]
+);
+
 /**
  *  末尾のスラッシュを取り除く
  */
---
+++ app/controllers/EntryController.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ *  [API] アカウントに関するクラス
+ *
+ *  アカウントに関するAPIをまとめたコントローラー
+ *  エンドポイント単位でメゾットを定義
+ *  Privateメゾットはプリフィックスにアンダーバー[_]をつける
+ *
+ *  @access public
+ *  @author Goma::NanoHa <goma@goma-gz.net>
+ *  @extends ControllerBase
+ */
+
+class EntryController extends ControllerBase
+{
+
+    public function addAction()
+    {
+
+        if($this->_status['response']['status'] && $this->_checkToken()) {
+            $this->_status['response']['status'] = false;
+            $this->_status['response']['code'] = 301;
+        }
+
+        $post = [
+            'entry' => true
+        ];
+        if($this->_status['response']['status'] && !$this->_getPost($post)) {
+            $this->_status['response']['status'] = false;
+            $this->_status['response']['code'] = 201;
+            $this->_status['response']['detail'] = $post['empty'];
+        }
+
+        $templateList = [
+            'title' => null,
+            'tag' => null,
+            'category' => null,
+            'content' => null,
+        ];
+        $conditions = [
+            'content'
+        ];
+        if($this->_status['response']['status'] && !$this->_mergeArray($this->_post['entry'], $templateList, $conditions)) {
+            $this->_status['response']['status'] = false;
+            $this->_status['response']['code'] = 202;
+            $this->_status['response']['detail'] = $conditions;
+        }
+
+        if(!$this->_status['response']['status']) {
+            return $this->response->setJsonContent($this->_status);
+        }
+
+        $posts = new Posts();
+        $posts->assign(
+            [
+                'author' => $this->_id,
+                'title' => $this->_post['entry']['title'],
+                'content' => $this->_post['entry']['content']
+            ]
+        );
+
+        if(!$posts->save()) {
+            $this->_status['response']['status'] = false;
+            $this->_status['response']['code'] = 102;
+            return $this->response->setJsonContent($this->_status);
+        }
+
+        if(!empty($this->_post['entry']['tag'])) {
+
+            if(is_string($this->_post['entry']['tag'])) $this->_post['entry']['tag'] = (array)$this->_post['entry']['tag'];
+
+            foreach($this->_post['entry']['tag'] as $tag) {
+
+                $tags = new Tags();
+                $tags->assign(
+                    [
+                        'posts_id' => $posts->id,
+                        'tag' => $tag
+                    ]
+                );
+
+                if(!$tags->save()) {
+                    $this->_status['response']['status'] = false;
+                    $this->_status['response']['code'] = 102;
+                    return $this->response->setJsonContent($this->_status);
+                }
+
+            }
+
+        }
+
+
+        if(!empty($this->_post['entry']['category'])) {
+
+            if(is_string($this->_post['entry']['category'])) $this->_post['entry']['category'] = (array)$this->_post['entry']['category'];
+
+            foreach($this->_post['entry']['category'] as $category) {
+
+                $categories = new Categories();
+                $categories->assign(
+                    [
+                        'posts_id' => $posts->id,
+                        'category' => $category
+                    ]
+                );
+
+                if(!$categories->save()) {
+                    $this->_status['response']['status'] = false;
+                    $this->_status['response']['code'] = 102;
+                    return $this->response->setJsonContent($this->_status);
+                }
+
+            }
+
+        }
+
+        return $this->response->setJsonContent($this->_status);
+
+    }
+
+    public function allAction()
+    {
+
+        
+
+    }
+
+    /**
+     *  Jsonのマージ及び必要項目のNullチェック
+     *
+     *  @access private
+     *  @param JSON &$json
+     *      リスト
+     *  @param array $templateList
+     *      リストのテンプレート
+     *  @param array &$conditions
+     *      リストの必要項目
+     *  @return boolean
+     */
+    private function _mergeArray(&$json, $templateList, &$conditions)
+    {
+        $array = json_decode($json, true);
+        $json = array_merge($templateList, $array);
+        $status = [];
+        foreach($conditions as $key) {
+            if(empty($json[$key])) $status[] = $key;
+        }
+        $conditions = $status;
+        return empty($status);
+    }
+}
+
